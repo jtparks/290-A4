a1fd955c941b772ed9cce61b798dba0d1fa3b312
Justin Parks <parksju@flip2.engr.oregonstate.edu>
2018-05-21 22:37:25 -0700

    Finished! I could not use port 3000, but tested on 3001. Hope it doesn't make a difference

--- public/index.js
+++ public/index.js
@@ -1,6 +1,105 @@
 /*
- * Add the contents of your index.js file from Assignment 3 here to see the
- * interactions you implemented.
+ * Add your JavaScript to this file to complete the assignment.
  */
 
-alert('Add the content of your index.js from Assignment 3 to public/index.js.');
+document.getElementById('create-twit-button').addEventListener("click", unhide);
+document.getElementById('navbar-search-button').addEventListener("click", searchquery);
+var canc = document.getElementsByClassName("modal-cancel-button");
+var clos = document.getElementsByClassName("modal-close-button");
+var acce = document.getElementsByClassName("modal-accept-button");
+canc[0].addEventListener("click", hide);
+clos[0].addEventListener("click", hide);
+acce[0].addEventListener("click", errorcheck);
+
+
+function unhide() {
+	
+	var hideclass = document.getElementsByClassName("hidden");
+	while (hideclass.length > 0) {
+		hideclass[0].classList.add("h");
+		hideclass[0].classList.remove("hidden");
+	}
+}
+
+
+function errorcheck() {
+	var bool;
+	if(document.getElementById("twit-text-input").value === "") {
+		bool = "false";
+	}
+
+	if(document.getElementById("twit-attribution-input").value ==="") {
+		bool = "false";
+	}
+	if(bool === "false") {
+		alert ("Please fill in both boxes!");
+	}
+	else {
+	post();
+	}
+}
+
+
+function post() {
+
+	var posty = document.getElementsByClassName("twit-container")[0];
+	
+	var ad = document.createElement("article");
+	ad.classList.add("twit");
+	
+	var plus = document.createElement("div");
+	plus.classList.add("twit-icon");
+	ad.appendChild(plus);
+	
+	var plus1 = document.createElement("i");
+	plus1.setAttribute("class","fa fa-bullhorn");
+	plus.appendChild(plus1);
+
+	var plus = document.createElement("div");
+	plus.classList.add("twit-content");
+	ad.appendChild(plus);
+
+	var plus1 = document.createElement("p");
+	plus1.classList.add("twit-text");
+
+	var text = document.getElementById("twit-text-input");
+	plus1.textContent = text.value;	
+	
+	plus.appendChild(plus1);
+
+	var plus1 = document.createElement("p");
+	plus1.classList.add("twit-attribution");
+
+	var text = document.getElementById("twit-attribution-input");
+	plus1.textContent = text.value;
+
+	plus.appendChild(plus1);
+
+	posty.appendChild(ad);
+
+	hide();
+}
+
+
+function hide() {
+	var showclass = document.getElementsByClassName("h");
+	
+	while (showclass.length > 0) {
+		showclass[0].classList.add("hidden");
+		showclass[0].classList.remove("h");
+	}
+
+	var clear = document.getElementById("twit-text-input").value = "";
+	var clear = document.getElementById("twit-attribution-input").value = "";
+}
+
+function searchquery() {
+	var twits = document.querySelectorAll('.twit');
+	var q = document.getElementById("navbar-search-input");
+	for (var i = 0; i < twits.length; i++) {
+		if(twits[i].textContent.toLowerCase().indexOf(q.value.toLowerCase()) == -1){
+			twits[i].parentNode.removeChild(twits[i]);
+		}
+	}
+}
+
--- server.js
+++ server.js
@@ -1,3 +1,55 @@
 /*
- * Write your server code in this file.
+ * Justin Parks
+ * parksju@oregonstate.edu
+ * Github Name is jtparks
  */
+
+var http = require("http");
+var fs   = require("fs");
+var path = require("path");
+var port = process.env.PORT;
+
+if(!port) {
+	port = 3000;					//DO NOT SUBMIT UNTIL SWITCHING DEFAULT TO 3000
+}
+
+let server = http.createServer((req, res) => {
+
+	console.log("  == Got a request");
+	console.log("    -- url:", req.url);
+
+	var pubpath = path.join(__dirname, 'public');
+
+	if ((req.url === '/') || (req.url === '/index')) {
+	
+		req.url = '/index.html';
+	}
+
+	var fullpath = path.join(pubpath, req.url);
+
+	fs.readFile(fullpath, (err, data) =>
+	{
+		if (err) {
+			res.statusCode = 404;
+			req.url = '/404.html';
+			var fullpath = path.join(pubpath, req.url);
+			fs.readFile(fullpath, (err, data) => {
+				res.end(data);
+			});
+		} else {
+			res.statusCode = 200;
+			res.end(data);
+		}
+		
+	});
+});
+
+server.listen(port, (err) => {
+       if(err) {
+	       console.log("Error! Something screwed up!");
+       }
+       else {
+	       console.log("== Using Port", port);
+       }
+});
+
